8377a135f3d57530107b698b172e2c68fe12be4c
AmandineF <amandinefouillet@gmail.com>
2015-03-23 15:25:41 -0400

    identification plus rapide

--- defense.py
+++ defense.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
 # coding: utf-8
 
+from scapy.all import *
+
 def mainDefense():
--- identifier.py
+++ identifier.py
@@ -5,29 +5,24 @@ from scapy.all import *
 import netaddr
 
 def identification():
-	conf.checkIPaddr = False
-	listIP = []
-	network = "192.168.0.0/24"
-	addresses = netaddr.IPNetwork(network)
-	liveCounter = 0
-	for host in addresses:
-		if (host == addresses.network or host == addresses.broadcast):
-			continue
-		resp = sr1(IP(dst=str(host))/ICMP(),timeout=0.000001,verbose=0)
-		if (str(type(resp)) == "<type 'NoneType'>"):
-			print str(host) + " is down or not responding."
-		elif (int(resp.getlayer(ICMP).type)==3 and int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
-		    print str(host) + " is blocking ICMP."
-		else:
-			print str(host) + " is responding."
-			#On met dans une liste les adresses IP qui répondent
-			listIP.append(str(host))
-			liveCounter += 1
+	#a revoir pour automatiser ??
+	network = '192.168.0.0/24'
 
-	#On affiche les adresses IP qui répondent
-	i = 0
-	while i < liveCounter:
-		print listIP[i]
-		i+=1
+	#Crée et envoie des paquets ARP
+	rec,unans=srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=network),timeout=10)
+
+	MACIP = []
+	#On enregistre le résultat
+	for send,recv in rec:
+		couple = (recv.sprintf(r'%Ether.src%'), recv.sprintf(r'%ARP.psrc%'))
+		MACIP.append(couple) 
+	#On affiche le résultat
+	i=0
+	while i < len(MACIP):
+		mac, ip = MACIP[i]
+		print 'MAC : ' + mac + ' <-> IP : ' + ip
+		i +=1
+
+
 
 identification()

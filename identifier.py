8de4794f5105a61abc622fb457a0a884b3ae7178
AmandineF <amandinefouillet@gmail.com>
2015-03-23 14:44:41 -0400

    dff

---
+++ identifier.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# coding: utf-8
+
+from scapy.all import *
+import netaddr
+
+def identification():
+	conf.checkIPaddr = False
+	listIP = []
+	listNIP = []
+	network = "192.168.0.0/24"
+	addresses = netaddr.IPNetwork(network)
+	liveCounter = 0
+	for host in addresses:
+		if (host == addresses.network or host == addresses.broadcast):
+			continue
+		resp = sr1(IP(dst=str(host))/ICMP(),timeout=0.000001,verbose=0)
+		if (str(type(resp)) == "<type 'NoneType'>"):
+			print str(host) + " is down or not responding."
+			listNIP.append(str(host))
+		elif (int(resp.getlayer(ICMP).type)==3 and int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
+		    print str(host) + " is blocking ICMP."
+		    listNIP.append(str(host))
+		else:
+			print str(host) + " is responding."
+			listIP.append(str(host))
+			liveCounter += 1
+
+	i = 0
+	while i < liveCounter:
+		print listIP[i]
+		i+=1
+
+	print "Out of " + str(addresses.size) + " hosts, " + str(liveCounter) + " are online."
+
+identification()
